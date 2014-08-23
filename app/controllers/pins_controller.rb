46af46e496cee0435eb85364f84aa49016095491
romy misra <romymisra@gmail.com>
2014-08-23 01:55:24 -0700

    adding images with paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,6 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
 
 
-
   def index
     @pins = Pin.all
   end
@@ -20,7 +19,9 @@ class PinsController < ApplicationController
   end
 
   def create
+    
     @pin = current_user.pins.build(pin_params)
+
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
